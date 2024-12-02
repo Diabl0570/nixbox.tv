e24dcd53c644abbe91ffc2bf9e5d577c1fb33a3f
Kasper Sprengers <Diabl0570@hotmail.com>
2024-12-02 19:46:31 +0000

    transmission peers queue updated

--- my-services/transmission.nix
+++ my-services/transmission.nix
@@ -1,6 +1,7 @@
 {
   services.transmission = { 
     enable = true; #Enable transmission daemon
+    openFirewall = true;
     openRPCPort = true; #Open firewall for RPC
     group = "Media";
     settings = { #Override default settings
@@ -11,12 +12,15 @@
       incomplete-dir-enabled = true;
       umask = 2;  # This sets the umask for new files (0002 in octal)
       # download-dir-permissions = "775";  # rwxrwxr-x # use umask instead
-      peer-limit-per-torrent = 50;
-      max-peers-global = 500;
+      peer-limit-per-torrent = 100;
+      max-peers-global = 5000; #old version
+      peer-limit-global = 5000;
       download-queue-enabled = true; #Boolean (default = true) When true, Transmission will only download download-queue-size non-stalled torrents at once.
-      download-queue-size = 10; #Number (default = 5) See download-queue-enabled
+      download-queue-size = 50; #Number (default = 5) See download-queue-enabled
       seed-queue-enabled = true;
-      seed-queue-size = 20; #Number (default = 10) See seed-queue-enabled.
+      seed-queue-size = 100; #Number (default = 10) See seed-queue-enabled.
+      cache-size-mb = 1024; #cache-size-mb: Number (default = 4), in megabytes, to allocate for Transmission's memory cache. The cache is used to help batch disk IO together, so increasing the cache size can be used to reduce the number of disk reads and writes. The value is the total available to the Transmission instance. Setting this to 0 bypasses the cache, which may be useful if your filesystem already has a cache layer that aggregates transactions.
+
     };
   };
 }
